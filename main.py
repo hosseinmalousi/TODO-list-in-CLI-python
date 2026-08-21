1ded0b7b20b6a0c6c6e1280d532639ff13fd386a
hosseinM <hossein.malosi@gmail.com>
2026-08-22 00:22:58 +0200

    refactor Task class and improve task listing format added datetime

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 def print_art():
     print()
     print(Asccii_art.art + "\n")
-    
+
 
 
 class Task:
@@ -18,27 +18,36 @@ class Task:
         self.task = task
         self.date = date
         self.desc = desc
-        Task.tasks.append({"uniq_code":self.uniq_code,"task":self.task,"desc":self.desc,"date":self.date})
+        self.done = False
+        Task.tasks.append({"uniq_code":self.uniq_code,"task":self.task,"desc":self.desc,"date":self.date , "done" : self.done})
     
     def __str__(self):
-        return f"the task : {self.task} is due to {self.date} with descirption of '{self.desc}'"
+        return f"the task : {self.task} is due to {datetime.date(self.date)}\ndescirption : '{self.desc}'"
         
     @classmethod
     def list_tasks(cls):
         print("***************")
         ### show it as a string and sort them or with bullet points
         for task in cls.tasks:
-            print(task)
+            print(f"{task["task"]} {(task["date"])} {task["desc"]} {task["done"]}")
         print("***************")
 
-    
+def add_task():
+    task_name=input("What do u wanna do champ ? 🎖️\n ")
+    ### date should be formed correctly
+    date=input("when do u wanna do it (DD-MM-YYYY)? (leave blank for no date)\n")
+    if date :
+        date = datetime.datetime.strptime(date,"%d-%m-%Y").date()
+    desc=input("is there any decription ? (enter to leave it blank) \n")
+    Task(task_name,desc,date)
+
 def main():
     print_art()
     is_running = True
     while is_running:
         print(f"today is {datetime.date.today().strftime("%A")} {datetime.date.today().strftime("%d:%m:%Y")}")
-        ### show it only when u have a task 
-        Task.list_tasks()
+        if Task.tasks:
+            Task.list_tasks()
         
         action = input("""1.tick a task \n2.add a task  \n3.delete a task \n4.edit a task \n5.quit the program\nplease add (1 , 2 , 3, 4)""")
         
@@ -47,10 +56,7 @@ def main():
                 pass
             case "2":
                 ### make it a func and show the tasks more beutiful
-                task_name=input("What do u wanna do champ ? 🎖️\n ")
-                date=input("when do u wanna do it ? (leave blank for no date)\n")
-                desc=input("is there any decription ? (enter to leave it blank) \n")
-                Task(task_name,desc,date)
+                add_task()
             case "3":
                 pass
             case "4":
